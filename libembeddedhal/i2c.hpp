aae70d722569d51e2aca5e0d184527f888beb3cc
Khalil Estell <kammce@google.com>
2021-09-15 12:16:11 -0700

    Change i2c duty cycle to unsigned int
    - The previous idea did not make much sense and was harder to perform
      arithmetic on then using an unsigned value.

--- libembeddedhal/i2c.hpp
+++ libembeddedhal/i2c.hpp
@@ -13,10 +13,10 @@ struct i2c_settings
 {
   /// Operating frequency
   uint32_t clock_rate_hz = 100'000;
-  /// Clock duty cycle, default to 50% which is represented as 0.
-  /// Positive values increase the duty cycle above 100% up to +127
-  /// Negative values decrease the duty cycle below 0% down to -128
-  full_scale<int8_t> duty_cycle = bit_depth<int8_t, 8>(0);
+  /// Clock duty cycle, default to 50% which is represented as 127.
+  /// Positive values increase the duty cycle above 100% up to 255.
+  /// Negative values decrease the duty cycle below 0% down to   0.
+  full_scale<uint8_t> duty_cycle = bit_depth<uint8_t, 8>(127);
 };
 
 /// An abstract interface for hardware that implements the Inter-integrated
