2303eacd7c6bff5910ff9b9e362f1f8cf3ebf5a6
Khalil Estell <kammce@google.com>
2021-09-18 15:19:20 -0700

    Add a is_a_test() method context.hpp
    - Platform defaults to "test" rather than unknown (a safer alternative
      than "unknown")
    - Add a is_a_test() method to make it easier for generic libraries to
      switch to unit test mode when

--- libembeddedhal/context.hpp
+++ libembeddedhal/context.hpp
@@ -4,7 +4,7 @@
 
 namespace embed {
 #if !defined(PLATFORM)
-#define PLATFORM_STRING "unknown"
+#define PLATFORM_STRING "test"
 #else
 #define STR_HELPER(x) #x
 #define STR(x) STR_HELPER(x)
@@ -16,5 +16,12 @@ constexpr const std::string_view platform_string = PLATFORM_STRING;
 constexpr bool is_platform(std::string_view p_platform)
 {
   return platform_string.starts_with(p_platform);
+}
+
+constexpr bool is_a_test()
+{
+  return (platform_string.starts_with("unit_test") ||
+          platform_string.starts_with("unittest") ||
+          platform_string.starts_with("test"));
 }
 }
