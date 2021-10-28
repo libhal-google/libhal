90c24cf9ae6d970c15048aa791266536e9898ff2
Khalil Estell <kammce@google.com>
2021-10-27 17:19:36 -0700

    Simplify names in clock interface

--- include/libembeddedhal/clock.hpp
+++ include/libembeddedhal/clock.hpp
@@ -7,26 +7,35 @@ namespace embed {
 class this_thread
 {
 public:
-  static void sleep_for(std::chrono::nanoseconds delay)
-  {
-    m_global_sleep(delay);
-  }
+  using time_increment = std::chrono::nanoseconds;
+  using sleep_function = std::function<void(std::chrono::nanoseconds)>;
+  using uptime_function = std::function<std::chrono::nanoseconds(void)>;
 
-  static void set_global_sleep(
-    std::function<void(std::chrono::nanoseconds)> sleep_function)
+  static auto sleep_for(time_increment delay) { return m_global_sleep(delay); }
+  static auto uptime() { return m_global_uptime(); }
+
+  static void set_global_sleep(sleep_function sleep_function)
   {
     m_global_sleep = sleep_function;
   }
+  static void set_global_uptime(uptime_function uptime_function)
+  {
+    m_global_uptime = uptime_function;
+  }
 
 private:
-  static inline auto m_loop_delay = [](std::chrono::nanoseconds loop_count) {
-    std::chrono::nanoseconds count{ 0 };
+  static inline auto m_incremental_uptime = []() -> time_increment {
+    static time_increment count{ 0 };
+    return count++;
+  };
+  static inline auto m_loop_delay = [](time_increment loop_count) {
+    time_increment count{ 0 };
     while (count < loop_count) {
       count += std::chrono::microseconds{ 1 };
     }
   };
 
-  static inline std::function<void(std::chrono::nanoseconds)> m_global_sleep =
-    m_loop_delay;
+  static inline sleep_function m_global_sleep = m_loop_delay;
+  static inline uptime_function m_global_uptime = m_incremental_uptime;
 };
 } // namespace embed
