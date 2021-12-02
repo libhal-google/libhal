59c497b38238b0aae65108e319a6feed6cf47132
Khalil Estell <kammce@google.com>
2021-12-02 06:16:42 -0800

    Document the interfaces and define clear behavior
    - Almost every interface has had its functions documented and the
      expected behavior listed.
    - Utility libraries have also had some of thier documentation filled
      out.
    - Move tests out of include directory.

--- include/libembeddedhal/static_callable.hpp
+++ include/libembeddedhal/static_callable.hpp
@@ -3,13 +3,26 @@
 #include <functional>
 
 namespace embed {
-// General class which will be used to allow for signature to be used and then
-// split by the below class.
+/**
+ * @brief General class which will be used to allow for signature to be used and
+ * then split by the below class.
+ *
+ * @tparam owner_class
+ * @tparam reference_designator
+ * @tparam signature
+ */
 template<class owner_class, int reference_designator, typename signature>
 class static_callable;
 
-// Specialization of the above class but with the return type and arguments
-// split up
+/**
+ * @brief Specialization of static_callable with the return type and arguments
+ * split up.
+ *
+ * @tparam owner_class see static_callable
+ * @tparam reference_designator see static_callable
+ * @tparam return_t
+ * @tparam args_t
+ */
 template<class owner_class,
          int reference_designator,
          typename return_t,
@@ -17,19 +30,38 @@ template<class owner_class,
 class static_callable<owner_class, reference_designator, return_t(args_t...)>
 {
 public:
+  /**
+   * @brief Construct a new static callable object
+   *
+   * @param p_callback
+   */
   static_callable(std::function<return_t(args_t...)> p_callback)
   {
     callback = p_callback;
   }
 
+  /**
+   * @brief Get the handler object
+   *
+   * @return auto*
+   */
   auto* get_handler() { return &handler; }
 
 private:
-  static return_t handler(args_t... args)
-  {
-    return callback(args...);
-  }
+  /**
+   * @brief private free function that calls the polymorphic callback. This is
+   * the function that can be used as entries in an interrupt vector tables or
+   * passed as a function pointer to C API.
+   *
+   * @param args list of arguments
+   * @return return_t the return type of the callback
+   */
+  static return_t handler(args_t... args) { return callback(args...); }
 
+  /**
+   * @brief the polymorphic callback to be
+   *
+   */
   inline static std::function<return_t(args_t...)> callback;
 };
 } // namespace embed
