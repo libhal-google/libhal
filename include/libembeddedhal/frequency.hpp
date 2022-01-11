185720bcd84a8e6dd23ef279545e92b99c3982d9
Khalil Estell <kammcecorp@gmail.com>
2022-01-11 06:42:18 -0800

    Integer based frequncy divider algorithms
    - Add initial documentation regarding floating point arithmetic and the
      cost of its usage.
    - Unit test for divider algorithm
    - Algorithms for calculating clock dividers for frequencies, frequencies
      and period values, and duty cycle period values.
    - Created frequency type to represent clock frequencies
    - Add math.hpp to include commonly used math functions such as rounding
      integer division and a generic absolute value function.
    
    Resolves #54

---
+++ include/libembeddedhal/frequency.hpp
@@ -0,0 +1,353 @@
+#pragma once
+
+#include <algorithm>
+#include <chrono>
+#include <cstdint>
+#include <limits>
+#include <ratio>
+
+#include "full_scale.hpp"
+#include "math.hpp"
+
+namespace embed {
+/**
+ * @brief Structure containing cycle count for the high and low side of a signal
+ * duty cycle.
+ *
+ */
+struct duty_cycle
+{
+  /// Number of cycles the signal will stay in the HIGH voltage state
+  std::int64_t high = 0;
+  /// Number of cycles the signal will stay in the LOW voltage state
+  std::int64_t low = 0;
+
+  /**
+   * @brief == operator
+   *
+   * @param p_cycle - other duty scale to compare to
+   * @return constexpr auto - true if the duty cycles have the exact same
+   * values.
+   */
+  constexpr auto operator==(const duty_cycle& p_cycle) const
+  {
+    return (high == p_cycle.high) && (low == p_cycle.low);
+  }
+};
+
+/**
+ * @brief Represents the frequency of a signal. It consists of a single integer
+ * 64-bit number that presents the integer representation of a signal frequency.
+ *
+ */
+class frequency
+{
+public:
+  /// Integer type used throughout the class
+  using int_t = std::int64_t;
+
+  /**
+   * @brief Generate a duty_cycle object based on the full_scale<T> value and
+   * the input count value. The count value is split based on the ratio within
+   * full_scale<T>
+   *
+   * @tparam T - the integral type of full_scale<T>
+   * @param p_cycles - the number of cycles to be split into a duty cycle
+   * @param p_precent - the target duty cycle percentage
+   * @return constexpr duty_cycle - the duty cycle cycle counts
+   */
+  template<std::integral T>
+  static constexpr duty_cycle calculate_duty_cycle(int_t p_cycles,
+                                                   full_scale<T> p_precent)
+  {
+    // Scale down value based on the integer percentage value in full_scale<T>
+    int_t high = p_cycles * p_precent;
+    // p_cycles will always be larger than or equal to high
+    int_t low = p_cycles - high;
+
+    return duty_cycle{
+      .high = high,
+      .low = low,
+    };
+  }
+
+  /**
+   * @brief construct the frequency object
+   *
+   * @param p_value - frequency of the object
+   */
+  explicit constexpr frequency(int_t p_value)
+    : m_cycles_per_second(p_value)
+  {}
+
+  /**
+   * @brief Get the frequency as an integer
+   *
+   * @return constexpr auto - frequency value as an integer
+   */
+  constexpr auto cycles_per_second() const
+  {
+    return absolute_value(m_cycles_per_second);
+  }
+
+  /**
+   * @brief Calculate the frequency divider required to generate the output
+   * frequency provided.
+   *
+   * @param p_target - the target output frequency
+   * @return constexpr int_t - the divider, when applied to this frequency, will
+   * achieve the p_target frequency. A value of is an error and 0 indicates that
+   * the output frequency is greater than this frequency and there does not
+   * exist an integer divider that can produce the output frequency.
+   */
+  constexpr int_t divider(frequency p_target) const
+  {
+    if (p_target.m_cycles_per_second > cycles_per_second()) {
+      return 0;
+    }
+    return rounding_division(cycles_per_second(), p_target.m_cycles_per_second);
+  }
+
+  /**
+   * @brief Calculate the number of cycles of this frequency within the time
+   * duration. This function is meant for timers to determine how many count
+   * cycles are needed to reach a paricular time duration at this frequency.
+   *
+   * @tparam Rep - type of the duration
+   * @tparam Period - ratio of the time duration relative to 1 second
+   * @param p_duration - the target time duration to get a cycle count from
+   * @return constexpr int_t - the number of cycles of this frequency within the
+   * duration.
+   */
+  template<typename Rep, typename Period>
+  constexpr int_t cycles_per(
+    std::chrono::duration<Rep, Period> p_duration) const
+  {
+    // Full Equation:
+    // =========================================================================
+    //
+    //                              / ratio_num \_
+    //   frequency_hz * |period| * | ----------- |  = cycles
+    //                              \ ratio_den /
+    //
+
+    // Make sure duration is a positive as the cycle count will always be a
+    // positive number.
+    const std::intmax_t absolute_duration = std::imaxabs(p_duration.count());
+    // The number of cycles between an input frequency and a duration can be
+    // found by multiplying the frequency by the time duration. The units cancel
+    // out and you get the number of clock intervals within a time period.
+    std::intmax_t cycle_count = cycles_per_second() * absolute_duration;
+    // The value is multiplied up before division to prevent as much lose of
+    // accuracy as possible.
+    cycle_count = rounding_division((cycle_count * Period::num), Period::den);
+
+    return static_cast<int_t>(cycle_count);
+  }
+
+  /**
+   * @brief Calculate a duty cycle based on a time duration, the full_scale<T>
+   * and this driving frequency. Typically used for PWM or clock lines with
+   * controllable duty cycles for serial communication.
+   *
+   * @tparam T - containing type of the full_scale<T>
+   * @tparam Rep - containing type of the duration
+   * @tparam Period - ratio of the time duration relative to 1 second
+   * @param p_duration - target time duration to reach
+   * @param p_precent - ratio of the duty cycle high time
+   * @return constexpr duty_cycle
+   */
+  template<std::integral T, typename Rep, typename Period>
+  constexpr duty_cycle calculate_duty_cycle(
+    std::chrono::duration<Rep, Period> p_duration,
+    full_scale<T> p_precent) const
+  {
+    return calculate_duty_cycle(cycles_per(p_duration), p_precent);
+  }
+
+  /**
+   * @brief Calculate a duty cycle based on a target, the full_scale<T>
+   * and this driving frequency. Typically used for PWM or clock lines with
+   * controllable duty cycles for serial communication.
+   *
+   * @tparam T - containing type of the full_scale<T>
+   * @param p_target - target frequency to reach
+   * @param p_precent - ratio of the duty cycle high time
+   * @return constexpr duty_cycle
+   */
+  template<std::integral T>
+  constexpr duty_cycle calculate_duty_cycle(frequency p_target,
+                                            full_scale<T> p_precent) const
+  {
+    return calculate_duty_cycle(divider(p_target), p_precent);
+  }
+
+  /**
+   * @brief Default operators for <, <=, >, >= and ==
+   *
+   * @return auto - result of the comparison
+   */
+  constexpr auto operator<=>(const frequency&) const = default;
+
+  /**
+   * @brief sum two frequencies together
+   *
+   * @param p_lhs - left hand frequency
+   * @param p_rhs - right hand frequency
+   * @return constexpr frequency - sum of the two frequencies
+   */
+  constexpr friend frequency operator+(frequency p_lhs, frequency p_rhs)
+  {
+    return frequency{ p_lhs.cycles_per_second() + p_rhs.cycles_per_second() };
+  }
+
+  /**
+   * @brief subtract two frequencies from each other
+   *
+   * @param p_lhs - left hand frequency
+   * @param p_rhs - right hand frequency
+   * @return constexpr frequency - difference of two frequencies
+   */
+  constexpr friend frequency operator-(frequency p_lhs, frequency p_rhs)
+  {
+    return frequency{ p_lhs.cycles_per_second() - p_rhs.cycles_per_second() };
+  }
+
+  /**
+   * @brief Scale up a frequency by an integer factor
+   *
+   * @tparam Integer - type of the integer
+   * @param p_lhs - the frequency to be scaled
+   * @param p_rhs - the integer value to scale the frequency by
+   * @return constexpr frequency
+   */
+  template<std::integral Integer>
+  constexpr friend frequency operator*(frequency p_lhs, Integer p_rhs)
+  {
+    return frequency{ p_lhs.cycles_per_second() * p_rhs };
+  }
+
+  /**
+   * @brief Scale up a frequency by an integer factor
+   *
+   * @tparam Integer - type of the integer
+   * @param p_lhs - the integer value to scale the frequency by
+   * @param p_rhs - the frequency to be scaled
+   * @return constexpr frequency
+   */
+  template<std::integral Integer>
+  constexpr friend frequency operator*(Integer p_rhs, frequency p_lhs)
+  {
+    return p_lhs * p_rhs;
+  }
+
+  /**
+   * @brief Scale down a frequency by an integer factor
+   *
+   * @tparam Integer - type of the integer
+   * @param p_lhs - the frequency to be scaled
+   * @param p_rhs - the integer value to scale the frequency by
+   * @return constexpr frequency
+   */
+  template<std::integral Integer>
+  constexpr friend frequency operator/(frequency p_lhs, Integer p_rhs)
+  {
+    return frequency{ rounding_division(p_lhs.cycles_per_second(),
+                                        int_t{ p_rhs }) };
+  }
+
+  /**
+   * @brief Scale down a frequency by an integer factor
+   *
+   * @param p_input - the input frequency to be divided down to the target
+   * frequency with an integer divider
+   * @param p_target - the target frequency to reach via an integer divider
+   * @return constexpr int_t - frequency divider value representing the number
+   * of cycles in the input that constitute one cycle in the target frequency.
+   */
+  constexpr friend int_t operator/(frequency p_input, frequency p_target)
+  {
+    return p_input.divider(p_target);
+  }
+
+  /**
+   * @brief multiplying a frequency by a time period returns the number of
+   * cycles of the input frequency contained within the time duration.
+   *
+   * @tparam Rep - type of the duration
+   * @tparam Period - ratio of the time duration relative to 1 second
+   * @param p_input - the input frequency
+   * @param p_duration - the target time duration to get a cycle count from
+   * @return constexpr int_t - the number of cycles of this frequency within the
+   * duration.
+   */
+  template<typename Rep, typename Period>
+  constexpr friend int_t operator*(
+    frequency p_input,
+    std::chrono::duration<Rep, Period> p_duration)
+  {
+    return p_input.cycles_per(p_duration);
+  }
+
+  /**
+   * @brief multiplying a frequency by a time period returns the number of
+   * cycles of the input frequency contained within the time duration.
+   *
+   * @tparam Rep - type of the duration
+   * @tparam Period - ratio of the time duration relative to 1 second
+   * @param p_input - the input frequency
+   * @param p_duration - the target time duration to get a cycle count from
+   * @return constexpr int_t - the number of cycles of this frequency within the
+   * duration.
+   */
+  template<typename Rep, typename Period>
+  constexpr friend int_t operator*(
+    std::chrono::duration<Rep, Period> p_duration,
+    frequency p_input)
+  {
+    return p_input * p_duration;
+  }
+
+private:
+  int_t m_cycles_per_second;
+};
+
+namespace literals {
+/**
+ * @brief user defined literals for making frequencies: 1337_Hz
+ *
+ * @param p_value
+ * @return constexpr frequency
+ */
+constexpr frequency operator""_Hz(unsigned long long p_value)
+{
+  return frequency{ static_cast<frequency::int_t>(p_value) };
+}
+
+/**
+ * @brief user defined literals for making frequencies in the kilohertz range:
+ * 20_kHz
+ *
+ * @param p_value
+ * @return constexpr frequency
+ */
+constexpr frequency operator""_kHz(unsigned long long p_value)
+{
+  const auto value = p_value * std::kilo::num;
+  return frequency{ static_cast<frequency::int_t>(value) };
+}
+
+/**
+ * @brief user defined literals for making frequencies in the megahertz range:
+ * 14_MHz
+ *
+ * @param p_value
+ * @return constexpr frequency
+ */
+constexpr frequency operator""_MHz(unsigned long long p_value)
+{
+  const auto value = p_value * std::mega::num;
+  return frequency{ static_cast<frequency::int_t>(value) };
+}
+}  // namespace literals
+}  // namespace embed
---
+++ tests/frequency.test.cpp
@@ -0,0 +1,146 @@
+#include <boost/ut.hpp>
+#include <libembeddedhal/frequency.hpp>
+
+namespace embed {
+
+std::ostream& operator<<(std::ostream& os, const frequency& p_frequency)
+{
+  return (os << p_frequency.cycles_per_second() << "_Hz");
+}
+
+std::ostream& operator<<(std::ostream& os, const duty_cycle& p_duty)
+{
+  return (os << "duty_cycle { high: " << p_duty.high << ","
+             << " low: " << p_duty.low << " }");
+}
+
+boost::ut::suite frequency_user_defined_literals_test = []() {
+  using namespace boost::ut;
+  using namespace embed::literals;
+
+  expect(that % 1'000'000 == (1_MHz).cycles_per_second());
+  expect(that % 8'000'000 == (8_MHz).cycles_per_second());
+  expect(that % 48'000'000 == (48_MHz).cycles_per_second());
+  expect(that % 133'000'000 == (133_MHz).cycles_per_second());
+  expect(that % 140'000'000 == (140_MHz).cycles_per_second());
+
+  expect(that % 12'000 == (12_kHz).cycles_per_second());
+  expect(that % 77'000 == (77_kHz).cycles_per_second());
+  expect(that % 53'000 == (53_kHz).cycles_per_second());
+  expect(that % 674'000 == (674_kHz).cycles_per_second());
+  expect(that % 927'000 == (927_kHz).cycles_per_second());
+  expect(that % 714'000 == (714_kHz).cycles_per_second());
+  expect(that % 714'834'000 == (714'834_kHz).cycles_per_second());
+
+  expect(that % 50 == (50_Hz).cycles_per_second());
+  expect(that % 1337 == (1337_Hz).cycles_per_second());
+  expect(that % 100 == (100_Hz).cycles_per_second());
+  expect(that % 1476 == (1476_Hz).cycles_per_second());
+  expect(that % 1290 == (1290_Hz).cycles_per_second());
+  expect(that % 8762 == (8762_Hz).cycles_per_second());
+};
+
+boost::ut::suite frequency_divider_test = []() {
+  using namespace boost::ut;
+  using namespace embed::literals;
+
+  expect(that % 333 == (1_MHz / 3_kHz));
+  expect(that % 11667 == (140_MHz / 12_kHz));
+  expect(that % 623 == (48_MHz / 77_kHz));
+  expect(that % 133 == (133_MHz / 1_MHz));
+  expect(that % 120'000 == (12_MHz / 100_Hz));
+  expect(that % 160'000 == (8_MHz / 50_Hz));
+  expect(that % 40 == (53_kHz / 1337_Hz));
+
+  // Result of zero should occur if the numerator is less than the denominator
+  expect(that % 0 == (10_kHz / 12_MHz));
+  expect(that % 0 == (50_MHz / 122_MHz));
+  expect(that % 0 == (100_Hz / 33_MHz));
+  expect(that % 0 == (667_kHz / 932_MHz));
+  expect(that % 0 == (1234_Hz / 54_MHz));
+};
+
+boost::ut::suite frequency_within_test = []() {
+  using namespace boost::ut;
+  using namespace std::literals;
+  using namespace embed::literals;
+
+  expect(that % 4 == (1_MHz).cycles_per(4us));
+  expect(that % 1680 == (140_MHz).cycles_per(12'000ns));
+  expect(that % 10 == (10_Hz).cycles_per(1s));
+  expect(that % 720 == (48_MHz).cycles_per(15us));
+  expect(that % 192 == (12_MHz).cycles_per(16us));
+  expect(that % 960'000'000 == (8_MHz).cycles_per(2min));
+  expect(that % 115'200'000 == (32_kHz).cycles_per(1h));
+
+  // Result of zero means that the time period is smaller than the frequency's
+  // period length
+  expect(that % 0 == (1_MHz).cycles_per(100ns));
+  expect(that % 0 == (100_Hz).cycles_per(1ms));
+  expect(that % 0 == (100_kHz).cycles_per(2us));
+};
+
+boost::ut::suite frequency_plus_minus_operator = []() {
+  using namespace boost::ut;
+  using namespace std::literals;
+  using namespace embed::literals;
+
+  expect(eq(1100_kHz, (1_MHz + 100_kHz)));
+  expect(eq(15'001_Hz, (15_kHz + 1_Hz)));
+  expect(eq(14'999_Hz, (15_kHz - 1_Hz)));
+  expect(eq(1'015_kHz, (15_kHz + 1_MHz)));
+};
+
+boost::ut::suite frequency_scalar_operator = []() {
+  using namespace boost::ut;
+  using namespace std::literals;
+  using namespace embed::literals;
+
+  expect(eq(2_MHz, (1_MHz * 2)));
+  expect(eq(45_kHz, (15_kHz * 3)));
+  expect(eq(5148_kHz, (78 * 66_kHz)));
+  expect(eq(32_MHz, (32 * 1_MHz)));
+
+  expect(eq(500_kHz, (1_MHz / 2)));
+  expect(eq(5_kHz, (15_kHz / 3)));
+  expect(eq(2143_Hz, (15_kHz / 7)));
+  expect(eq(11_kHz, (66_kHz / 6)));
+  expect(eq(31250_Hz, (1_MHz / 32)));
+};
+
+boost::ut::suite frequency_duty_cycle_test = []() {
+  using namespace boost::ut;
+  using namespace std::literals;
+  using namespace embed::literals;
+
+  expect(eq(duty_cycle{ 280000, 0 },
+            (14_MHz).calculate_duty_cycle(20ms, full_scale<uint32_t>(1.00))));
+  expect(eq(duty_cycle{ 277200, 2800 },
+            (14_MHz).calculate_duty_cycle(20ms, full_scale<uint32_t>(0.99))));
+  expect(eq(duty_cycle{ 238000, 42000 },
+            (14_MHz).calculate_duty_cycle(20ms, full_scale<uint32_t>(0.85))));
+  expect(eq(duty_cycle{ 126000, 154000 },
+            (14_MHz).calculate_duty_cycle(20ms, full_scale<uint32_t>(0.45))));
+  expect(eq(duty_cycle{ 70'000, 210'000 },
+            (14_MHz).calculate_duty_cycle(20ms, full_scale<uint32_t>(0.25))));
+  expect(eq(duty_cycle{ 14000, 266000 },
+            (14_MHz).calculate_duty_cycle(20ms, full_scale<uint32_t>(0.05))));
+  expect(eq(duty_cycle{ 2800, 277200 },
+            (14_MHz).calculate_duty_cycle(20ms, full_scale<uint32_t>(0.01))));
+  expect(eq(duty_cycle{ 0, 280000 },
+            (14_MHz).calculate_duty_cycle(20ms, full_scale<uint32_t>(0.00))));
+
+  expect(eq(duty_cycle{ 2800, 0 },
+            (56_MHz).calculate_duty_cycle(20_kHz, full_scale<uint32_t>(1.00))));
+  expect(eq(duty_cycle{ 2240, 560 },
+            (56_MHz).calculate_duty_cycle(20_kHz, full_scale<uint32_t>(0.80))));
+  expect(eq(duty_cycle{ 1540, 1260 },
+            (56_MHz).calculate_duty_cycle(20_kHz, full_scale<uint32_t>(0.55))));
+  expect(eq(duty_cycle{ 924, 1876 },
+            (56_MHz).calculate_duty_cycle(20_kHz, full_scale<uint32_t>(0.33))));
+  expect(eq(duty_cycle{ 336, 2464 },
+            (56_MHz).calculate_duty_cycle(20_kHz, full_scale<uint32_t>(0.12))));
+  expect(eq(duty_cycle{ 0, 2800 },
+            (56_MHz).calculate_duty_cycle(20_kHz, full_scale<uint32_t>(0.00))));
+};
+}  // namespace embed
