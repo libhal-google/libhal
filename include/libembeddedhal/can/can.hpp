4825a3d5278c55ac9527b8b64cd957f2b99a51cd
Khalil Estell <kammcecorp@gmail.com>
2022-01-20 08:20:34 -0800

    Remove reception functions from embed::can
    The previous function signature for attach_interrupt() begs the
    question of “who clears the interrupt handler and when”? On many
    systems the way it is cleared is either to read the data out of
    the FIFO/mailbox/registers or set a bit that clears the FIFOs.
    
    This leaves a few options:
    
    - User defined handler are REQUIRED to read the buffers or be stuck
      in a loop.
    - When a handler occurs, user defined handler is called, when finished
      service routine will clear all messages in the buffer.
    - Pass messages to handler, clear message after usage.
    
    The last choice is the preferred option because the handler has no
    obligation to read the buffer or even access the can object and
    clearing of each message can be handled by the service routine.
    
    The reception functions were removed because they no longer useful
    with this new API since has_data() and reception will always return
    empty if an interrupt services these first.

--- include/libembeddedhal/can/can.hpp
+++ include/libembeddedhal/can/can.hpp
@@ -54,23 +54,6 @@ public:
   virtual boost::leaf::result<void> send(const message_t& p_message) = 0;
 
   /**
-   * @brief Determine if the can bus has any queued up can messages
-   *
-   * @return boost::leaf::result<bool> - true if a can message is available to
-   * read, false otherwise.
-   */
-  virtual boost::leaf::result<bool> has_data() = 0;
-
-  /**
-   * @brief Receieve a message from the can bus if one is available.
-   *
-   * Behavior is undefined if no data is available.
-   *
-   * @return boost::leaf::result<message_t> - a can message from the bus
-   */
-  virtual boost::leaf::result<message_t> receive() = 0;
-
-  /**
    * @brief Setup driver to execute callback when a can message is received.
    *
    * @param p_receive_handler - this handler will be called when the can device
