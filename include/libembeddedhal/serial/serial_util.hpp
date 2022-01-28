7daa437b98fbd13449b87793fe10b11a4f9946f1
Khalil Estell <kammcecorp@gmail.com>
2022-01-28 04:40:33 -0800

    Add serial_util.hpp

---
+++ include/libembeddedhal/serial/serial_util.hpp
@@ -0,0 +1,87 @@
+#pragma once
+
+#include <optional>
+#include <span>
+#include <string_view>
+
+#include "../error.hpp"
+#include "../time.hpp"
+#include "serial.hpp"
+
+namespace embed {
+
+inline boost::leaf::result<void> wait(
+  serial& p_serial,
+  size_t p_length,
+  std::optional<std::chrono::nanoseconds> p_timeout = std::nullopt)
+{
+  if (p_timeout) {
+    auto current_time = this_thread::uptime();
+    auto timeout_time = current_time + p_timeout.value();
+    size_t bytes_available = EMBED_CHECK(p_serial.bytes_available());
+
+    while (bytes_available < p_length) {
+      current_time = this_thread::uptime();
+      if (current_time > timeout_time) {
+        return boost::leaf::new_error(error::timeout{});
+      }
+      bytes_available = EMBED_CHECK(p_serial.bytes_available());
+    }
+  } else {
+    size_t bytes_available = EMBED_CHECK(p_serial.bytes_available());
+    while (bytes_available < p_length) {
+      bytes_available = EMBED_CHECK(p_serial.bytes_available());
+    }
+  }
+
+  return {};
+}
+
+inline boost::leaf::result<void> write(serial& p_serial,
+                                       std::span<const std::byte> p_data_out)
+{
+  return p_serial.write(p_data_out);
+}
+
+inline boost::leaf::result<std::span<const std::byte>> read(
+  serial& p_serial,
+  std::span<std::byte> p_data_in,
+  std::optional<std::chrono::nanoseconds> p_timeout = std::nullopt)
+{
+  EMBED_CHECK(wait(p_serial, p_data_in.size(), p_timeout));
+  return p_serial.read(p_data_in);
+}
+
+template<size_t BytesToRead>
+inline boost::leaf::result<std::array<std::byte, BytesToRead>> read(
+  serial& p_serial,
+  std::optional<std::chrono::nanoseconds> p_timeout = std::nullopt)
+{
+  std::array<std::byte, BytesToRead> buffer;
+  EMBED_CHECK(wait(p_serial, BytesToRead, p_timeout));
+  EMBED_CHECK(p_serial.read(buffer));
+  return buffer;
+}
+
+inline boost::leaf::result<void> write_then_read(
+  serial& p_serial,
+  std::span<const std::byte> p_data_out,
+  std::span<std::byte> p_data_in,
+  std::optional<std::chrono::nanoseconds> p_timeout = std::nullopt)
+{
+  EMBED_CHECK(write(p_serial, p_data_out));
+  EMBED_CHECK(read(p_serial, p_data_in, p_timeout));
+  return {};
+}
+
+template<size_t BytesToRead>
+inline boost::leaf::result<std::array<std::byte, BytesToRead>> write_then_read(
+  serial& p_serial,
+  std::span<const std::byte> p_data_out,
+  std::optional<std::chrono::nanoseconds> p_timeout = std::nullopt)
+{
+  std::array<std::byte, BytesToRead> buffer;
+  EMBED_CHECK(write_then_read(p_serial, p_data_out, buffer, p_timeout));
+  return buffer;
+}
+}  // namespace embed
