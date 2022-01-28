f9dbbef315c3bb4a4a63a8476e079ff637d57525
Khalil Estell <kammcecorp@gmail.com>
2022-01-28 04:40:07 -0800

    Add spi_util.hpp

---
+++ tests/spi/spi_util.test.cpp
@@ -0,0 +1,211 @@
+#include <boost/ut.hpp>
+#include <libembeddedhal/spi/spi_util.hpp>
+#include <span>
+
+namespace embed {
+boost::ut::suite spi_util_test = []() {
+  using namespace boost::ut;
+
+  static constexpr std::byte success_filler{ 0xF5 };
+  static constexpr std::byte failure_filler{ 0x33 };
+  static constexpr std::byte filler_byte{ 0xA5 };
+
+  class dummy : public embed::spi
+  {
+  public:
+    boost::leaf::result<void> driver_initialize() override { return {}; }
+    boost::leaf::result<void> transfer(std::span<const std::byte> p_out,
+                                       std::span<std::byte> p_in,
+                                       std::byte p_filler) override
+    {
+      if (!p_out.empty()) {
+        m_out = p_out;
+      }
+
+      if (!p_in.empty()) {
+        m_in = p_in;
+        std::fill(m_in.begin(), m_in.end(), filler_byte);
+      }
+
+      m_filler = p_filler;
+
+      if (p_filler == failure_filler) {
+        return boost::leaf::new_error();
+      }
+
+      return {};
+    }
+
+    virtual ~dummy() {}
+
+    std::span<const std::byte> m_out = std::span<const std::byte>{};
+    std::span<std::byte> m_in = std::span<std::byte>{};
+    std::byte m_filler = std::byte{ 0xFF };
+  };
+
+  "[success] write"_test = []() {
+    // Setup
+    dummy spi;
+    const std::array<std::byte, 4> expected_payload{};
+
+    // Exercise
+    auto result = write(spi, expected_payload);
+    bool successful = static_cast<bool>(result);
+
+    // Verify
+    expect(successful);
+    expect(that % expected_payload.data() == spi.m_out.data());
+    expect(that % expected_payload.size() == spi.m_out.size());
+    expect(that % nullptr == spi.m_in.data());
+    expect(that % 0 == spi.m_in.size());
+  };
+
+  "[success] read"_test = []() {
+    // Setup
+    dummy spi;
+    std::array<std::byte, 4> expected_buffer;
+
+    // Exercise
+    auto result = read(spi, expected_buffer, success_filler);
+    bool successful = static_cast<bool>(result);
+
+    // Verify
+    expect(successful);
+    expect(success_filler == spi.m_filler);
+    expect(that % expected_buffer.data() == spi.m_in.data());
+    expect(that % expected_buffer.size() == spi.m_in.size());
+    expect(that % nullptr == spi.m_out.data());
+    expect(that % 0 == spi.m_out.size());
+  };
+
+  "[failure] read"_test = []() {
+    // Setup
+    dummy spi;
+    std::array<std::byte, 4> expected_buffer;
+
+    // Exercise
+    auto result = read(spi, expected_buffer, failure_filler);
+    bool successful = static_cast<bool>(result);
+
+    // Verify
+    expect(!successful);
+    expect(failure_filler == spi.m_filler);
+    expect(that % expected_buffer.data() == spi.m_in.data());
+    expect(that % expected_buffer.size() == spi.m_in.size());
+    expect(that % nullptr == spi.m_out.data());
+    expect(that % 0 == spi.m_out.size());
+  };
+
+  "[success] read<Length>"_test = []() {
+    // Setup
+    dummy spi;
+    std::array<std::byte, 5> expected_buffer;
+    expected_buffer.fill(filler_byte);
+
+    // Exercise
+    auto result = read<expected_buffer.size()>(spi, success_filler);
+    bool successful = static_cast<bool>(result);
+
+    // Verify
+    expect(successful);
+    expect(success_filler == spi.m_filler);
+    expect(std::equal(
+      expected_buffer.begin(), expected_buffer.end(), result.value().begin()));
+    expect(that % nullptr == spi.m_out.data());
+    expect(that % 0 == spi.m_out.size());
+  };
+
+  "[failure] read<Length>"_test = []() {
+    // Setup
+    dummy spi;
+
+    // Exercise
+    auto result = read<5>(spi, failure_filler);
+    bool successful = static_cast<bool>(result);
+
+    // Verify
+    expect(!successful);
+    expect(failure_filler == spi.m_filler);
+    expect(that % nullptr == spi.m_out.data());
+    expect(that % 0 == spi.m_out.size());
+  };
+
+  "[success] write_then_read"_test = []() {
+    // Setup
+    dummy spi;
+    const std::array<std::byte, 4> expected_payload{};
+    std::array<std::byte, 4> expected_buffer;
+
+    // Exercise
+    auto result =
+      write_then_read(spi, expected_payload, expected_buffer, success_filler);
+    bool successful = static_cast<bool>(result);
+
+    // Verify
+    expect(successful);
+    expect(success_filler == spi.m_filler);
+    expect(that % expected_payload.data() == spi.m_out.data());
+    expect(that % expected_payload.size() == spi.m_out.size());
+    expect(that % expected_buffer.data() == spi.m_in.data());
+    expect(that % expected_buffer.size() == spi.m_in.size());
+  };
+
+  "[failure] write_then_read"_test = []() {
+    // Setup
+    dummy spi;
+    const std::array<std::byte, 4> expected_payload{};
+    std::array<std::byte, 4> expected_buffer;
+    expected_buffer.fill(filler_byte);
+
+    // Exercise
+    auto result =
+      write_then_read(spi, expected_payload, expected_buffer, failure_filler);
+    bool successful = static_cast<bool>(result);
+
+    // Verify
+    expect(!successful);
+    expect(failure_filler == spi.m_filler);
+    expect(that % expected_payload.data() == spi.m_out.data());
+    expect(that % expected_payload.size() == spi.m_out.size());
+    expect(that % expected_buffer.data() == spi.m_in.data());
+    expect(that % expected_buffer.size() == spi.m_in.size());
+  };
+
+  "[success] write_then_read<Length>"_test = []() {
+    // Setup
+    dummy spi;
+    const std::array<std::byte, 4> expected_payload{};
+    std::array<std::byte, 4> expected_buffer{};
+    expected_buffer.fill(filler_byte);
+
+    // Exercise
+    auto result = write_then_read<5>(spi, expected_payload, success_filler);
+    bool successful = static_cast<bool>(result);
+    auto actual_array = result.value();
+
+    // Verify
+    expect(successful);
+    expect(success_filler == spi.m_filler);
+    expect(that % expected_payload.data() == spi.m_out.data());
+    expect(that % expected_payload.size() == spi.m_out.size());
+    expect(std::equal(
+      expected_buffer.begin(), expected_buffer.end(), actual_array.begin()));
+  };
+
+  "[failure] write_then_read<Length>"_test = []() {
+    // Setup
+    dummy spi;
+    const std::array<std::byte, 4> expected_payload{};
+
+    // Exercise
+    auto result = write_then_read<5>(spi, expected_payload, failure_filler);
+    bool successful = static_cast<bool>(result);
+
+    // Verify
+    expect(!successful);
+    expect(failure_filler == spi.m_filler);
+    expect(that % expected_payload.data() == spi.m_out.data());
+    expect(that % expected_payload.size() == spi.m_out.size());
+  };
+};
+}  // namespace embed
