7eb681a784b6024f3fea6329303489cb5fb62bf6
apre <herculanodavi@gmail.com>
2022-04-20 18:11:13 +0200

    Add embed::stub::spi tests

---
+++ tests/spi/spi_stub.test.cpp
@@ -0,0 +1,15 @@
+#include <boost/ut.hpp>
+#include <libembeddedhal/spi/spi_stub.hpp>
+#include <span>
+
+namespace embed {
+boost::ut::suite spi_util_test = []() {
+  using namespace boost::ut;
+
+  // Setup
+  embed::stub::spi stub;
+
+  expect(bool{stub.configure({})});
+  expect(bool{stub.transfer({}, {}, std::byte{0xFF})});
+};
+}  // namespace embed
