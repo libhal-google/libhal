fb8f459d931c42382b274bd4883430a4cbacaaa8
apre <herculanodavi@gmail.com>
2022-05-01 13:51:14 +0200

    Added embed::mock::write_only_spi

--- tests/spi/spi_mock.test.cpp
+++ tests/spi/spi_mock.test.cpp
@@ -6,7 +6,7 @@ namespace embed {
 boost::ut::suite spi_mock_test = []() {
   using namespace boost::ut;
 
-  "embed::mock::spi::configure()"_test = []() {
+  "embed::mock::write_only_spi::configure()"_test = []() {
     // Setup
     constexpr embed::spi::settings expected1 = {
         .clock_rate = frequency(1'000),
@@ -18,7 +18,7 @@ boost::ut::suite spi_mock_test = []() {
         .clock_idles_high = true,
         .data_valid_on_trailing_edge = true};
 
-    embed::mock::spi mock;
+    embed::mock::write_only_spi mock;
     mock.spy_configure.trigger_error_on_call(3);
 
     // Exercise + Verify
@@ -32,40 +32,26 @@ boost::ut::suite spi_mock_test = []() {
     expect(expected2 == std::get<0>(mock.spy_configure.call_history().at(2)));
   };
 
-  "embed::mock::spi::transfer()"_test = []() {
+  "embed::mock::write_only_spi::transfer()"_test = []() {
     // Setup
-    constexpr std::array<const std::byte, 1> data_out_expected1{std::byte{0xAA}};
-    std::array<std::byte, 1> data_in_expected1{std::byte{0xBB}};
-    constexpr std::byte filler_expected1{0xCC};
+    constexpr std::array<const std::byte, 1> out_1{std::byte{0xAA}};
+    constexpr std::array<const std::byte, 2> out_2{std::byte{0xDD},
+                                                   std::byte{0xCC}};
+    constexpr std::span<std::byte> dummy{};
+    constexpr std::byte filler{0xFF};
 
-    constexpr std::array<const std::byte, 1> data_out_expected2{std::byte{0xDD}};
-    std::array<std::byte, 1> data_in_expected2{std::byte{0xEE}};
-    constexpr std::byte filler_expected2{0xFF};
-
-    constexpr auto compare_span = [](auto lhs, auto rhs) -> bool {
-      return std::equal(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
-    };
-
-    embed::mock::spi mock;
-    mock.spy_transfer.trigger_error_on_call(3);
+    embed::mock::write_only_spi mock;
 
     // Exercise + Verify
-    expect(bool{mock.transfer(data_out_expected1, data_in_expected1,
-                              filler_expected1)});
-    expect(compare_span(data_out_expected1, std::get<0>(mock.spy_transfer.call_history().at(0))));
-    expect(compare_span(data_in_expected1, std::get<1>(mock.spy_transfer.call_history().at(0))));
-    expect(filler_expected1 == std::get<2>(mock.spy_transfer.call_history().at(0)));
+    expect(bool{mock.transfer(out_1, dummy, filler)});
+    expect(std::ranges::equal(out_1, mock.write_record.at(0)));
 
-    expect(bool{mock.transfer(data_out_expected2, data_in_expected2,
-                              filler_expected2)});
-    expect(compare_span(data_out_expected2, std::get<0>(mock.spy_transfer.call_history().at(1))));
-    expect(compare_span(data_in_expected2, std::get<1>(mock.spy_transfer.call_history().at(1))));
-    expect(filler_expected2 == std::get<2>(mock.spy_transfer.call_history().at(1)));
+    expect(bool{mock.transfer(out_2, dummy, filler)});
+    expect(std::ranges::equal(out_2, mock.write_record.at(1)));
+    expect(mock.write_record.size() == 2);
 
-    expect(!mock.transfer(data_out_expected2, data_in_expected2, filler_expected2));
-    expect(compare_span(data_out_expected2, std::get<0>(mock.spy_transfer.call_history().at(2))));
-    expect(compare_span(data_in_expected2, std::get<1>(mock.spy_transfer.call_history().at(2))));
-    expect(filler_expected2 == std::get<2>(mock.spy_transfer.call_history().at(2)));
+    mock.reset();
+    expect(mock.write_record.size() == 0);
   };
 };
 }  // namespace embed
