29174707b3843f31e8ce95200b4e60c220a87dce
MaliaLabor <malia.labor@hotmail.com>
2022-05-07 11:51:57 -0700

    Add embed::stub::adc
    Resolves #150

---
+++ tests/adc/adc_stub.test.cpp
@@ -0,0 +1,15 @@
+#include <boost/ut.hpp>
+#include <libembeddedhal/adc/adc_stub.hpp>
+
+namespace embed {
+boost::ut::suite adc_stub_test = []() {
+  using namespace boost::ut;
+
+  // Setup
+  auto expected = embed::percent::from_ratio(1, 2);
+  embed::stub::adc stub(expected);
+
+  // Exercise + Verify
+  expect(that % expected.raw_value() == stub.read().value().raw_value());
+};
+}  // namespace embed
