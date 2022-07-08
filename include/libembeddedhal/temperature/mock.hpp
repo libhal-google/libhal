03292e8c591478fbe8695873cec03ec872b605ad
MaliaLabor <malia.labor@hotmail.com>
2022-07-08 11:27:23 -0700

    Add embed::mock::temperature
    Resolves #161

---
+++ include/libembeddedhal/temperature/mock.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <queue>
+
+#include "interface.hpp"
+
+namespace embed::mock {
+/**
+ * @brief Mock temperature sensor implementation for use in unit tests and
+ * simulations.
+ *
+ */
+struct temperature_sensor : public embed::temperature_sensor
+{
+  /**
+   * @brief Queues the temperature values to be returned for read()
+   *
+   * @param p_temperatures - queue of temperatures
+   */
+  void set(std::queue<temperature>& p_temperatures)
+  {
+    m_temperatures = p_temperatures;
+  }
+
+private:
+  boost::leaf::result<temperature> driver_read() noexcept
+  {
+    if (m_temperatures.size() == 0) {
+      return boost::leaf::new_error(
+        std::out_of_range("temperatures queue is empty!"));
+    }
+    temperature m_current_value = m_temperatures.front();
+    m_temperatures.pop();
+    return m_current_value;
+  }
+
+  std::queue<temperature> m_temperatures{};
+};
+}  // namespace embed::mock
---
+++ tests/temperature/mock.test.cpp
@@ -0,0 +1,29 @@
+#include <deque>
+
+#include <boost/ut.hpp>
+#include <libembeddedhal/temperature/mock.hpp>
+
+namespace embed {
+boost::ut::suite temperature_mock_test = []() {
+  using namespace boost::ut;
+
+  "embed::mock::temperature::read()"_test = []() {
+    // Setup
+    embed::mock::temperature_sensor mock;
+    auto expected1 = temperature(0);
+    auto expected2 = temperature(1000);
+    auto expected3 = temperature(2000);
+    std::deque temps{ expected1, expected2, expected3 };
+    std::queue queue(temps);
+
+    // Exercise
+    mock.set(queue);
+
+    // Verify
+    expect(expected1 == mock.read().value());
+    expect(expected2 == mock.read().value());
+    expect(expected3 == mock.read().value());
+    expect(throws([&mock] { mock.read().value(); }));
+  };
+};
+}  // namespace embed
