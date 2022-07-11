9cfdfcee3cc6cea61f4ee58835e1d6cc94ccb580
MaliaLabor <malia.labor@hotmail.com>
2022-07-11 14:40:49 -0700

    Add embed::mock::interrupt_pin
    Resolves #3

---
+++ include/libembeddedhal/interrupt_pin/mock.hpp
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <queue>
+#include <stdexcept>
+
+#include "../testing.hpp"
+#include "interface.hpp"
+
+namespace embed::mock {
+/**
+ * @brief mock interrupt_pin implementation for use in unit tests and
+ * simulations.
+ *
+ */
+struct interrupt_pin : public embed::interrupt_pin
+{
+  /**
+   * @brief Reset spy information for configure(), attach_interrupt(), and
+   * detach_interrupt()
+   *
+   */
+  void reset()
+  {
+    spy_configure.reset();
+    spy_attach_interrupt.reset();
+    spy_detach_interrupt.reset();
+  }
+  /**
+   * @brief Queues the active levels to be returned for level()
+   *
+   * @param p_levels - queue of actives levels
+   */
+  void set(std::queue<bool>& p_levels)
+  {
+    m_levels = p_levels;
+  }
+
+  /// Spy handler for embed::interrupt_pin::configure()
+  spy_handler<settings> spy_configure;
+  /// Spy handler for embed::interrupt_pin::attach_interrupt()
+  spy_handler<std::function<void(void)>, trigger_edge> spy_attach_interrupt;
+  /// Spy handler for embed::interrupt_pin::detach_interrupt()
+  spy_handler<bool> spy_detach_interrupt;
+
+private:
+  boost::leaf::result<void> driver_configure(
+    const settings& p_settings) noexcept override
+  {
+    return spy_configure.record(p_settings);
+  }
+  boost::leaf::result<bool> driver_level() noexcept override
+  {
+    if (m_levels.size() == 0) {
+      return boost::leaf::new_error(
+        std::out_of_range("interrupt pin level queue is empty!"));
+    }
+    bool m_current_value = m_levels.front();
+    m_levels.pop();
+    return m_current_value;
+  }
+  boost::leaf::result<void> driver_attach_interrupt(
+    std::function<void(void)> p_callback,
+    trigger_edge p_trigger) noexcept override
+  {
+    return spy_attach_interrupt.record(p_callback, p_trigger);
+  }
+  boost::leaf::result<void> driver_detach_interrupt() noexcept override
+  {
+    return spy_detach_interrupt.record(true);
+  }
+
+  std::queue<bool> m_levels{};
+};
+}  // namespace embed::mock
---
+++ tests/interrupt_pin/mock.test.cpp
@@ -0,0 +1,106 @@
+#include <deque>
+
+#include <boost/ut.hpp>
+#include <libembeddedhal/interrupt_pin/mock.hpp>
+
+namespace embed {
+boost::ut::suite interrupt_pin_mock_test = []() {
+  using namespace boost::ut;
+
+  "embed::mock::interrupt_pin::configure()"_test = []() {
+    // Setup
+    constexpr embed::interrupt_pin::settings mock_settings_default{};
+    constexpr embed::interrupt_pin::settings mock_settings_custom{
+      .resistor = pin_resistor::pull_down,
+    };
+    embed::mock::interrupt_pin mock;
+
+    // Exercise
+    auto result1 = mock.configure(mock_settings_default);
+    auto result2 = mock.configure(mock_settings_custom);
+
+    // Verify
+    expect(bool{ result1 });
+    expect(bool{ result2 });
+    expect(mock_settings_default.resistor ==
+           std::get<0>(mock.spy_configure.call_history().at(0)).resistor);
+    expect(mock_settings_custom.resistor ==
+           std::get<0>(mock.spy_configure.call_history().at(1)).resistor);
+  };
+  "embed::mock::interrupt_pin::set() + level()"_test = []() {
+    // Setup
+    embed::mock::interrupt_pin mock;
+    std::deque inputs{ true, false, true };
+    std::queue queue(inputs);
+
+    // Exercise
+    mock.set(queue);
+
+    // Verify
+    expect(that % true == mock.level().value());
+    expect(that % false == mock.level().value());
+    expect(that % true == mock.level().value());
+    expect(throws([&mock] { mock.level().value(); }));
+  };
+  "embed::mock::interrupt_pin::detach_interrupt()"_test = []() {
+    // Setup
+    embed::mock::interrupt_pin mock;
+
+    // Exercise
+    (void)mock.detach_interrupt();
+
+    // Verify
+    expect(that % true ==
+           std::get<0>(mock.spy_detach_interrupt.call_history().at(0)));
+  };
+  "embed::mock::interrupt_pin::attach_interrupt()"_test = []() {
+    // Setup
+    const std::function<void(void)> expected_callback = []() {};
+    const embed::interrupt_pin::trigger_edge expected_falling =
+      embed::interrupt_pin::trigger_edge::falling;
+    const embed::interrupt_pin::trigger_edge expected_rising =
+      embed::interrupt_pin::trigger_edge::rising;
+    const embed::interrupt_pin::trigger_edge expected_both =
+      embed::interrupt_pin::trigger_edge::both;
+    embed::mock::interrupt_pin mock;
+
+    // Exercise
+    auto result1 = mock.attach_interrupt(expected_callback, expected_falling);
+    auto result2 = mock.attach_interrupt(expected_callback, expected_rising);
+    auto result3 = mock.attach_interrupt(expected_callback, expected_both);
+
+    // Verify
+    expect(bool{ result1 });
+    expect(bool{ result2 });
+    expect(bool{ result3 });
+    expect(expected_falling ==
+           std::get<1>(mock.spy_attach_interrupt.call_history().at(0)));
+    expect(expected_rising ==
+           std::get<1>(mock.spy_attach_interrupt.call_history().at(1)));
+    expect(expected_both ==
+           std::get<1>(mock.spy_attach_interrupt.call_history().at(2)));
+  };
+  "embed::mock::interrupt_pin::reset()"_test = []() {
+    // Setup
+    constexpr embed::interrupt_pin::settings mock_settings_default{};
+    const std::function<void(void)> expected_callback = []() {};
+    const embed::interrupt_pin::trigger_edge expected_trigger =
+      embed::interrupt_pin::trigger_edge::falling;
+    embed::mock::interrupt_pin mock;
+    (void)mock.configure(mock_settings_default);
+    expect(that % 1 == mock.spy_configure.call_history().size());
+    (void)mock.attach_interrupt(expected_callback, expected_trigger);
+    expect(that % 1 == mock.spy_attach_interrupt.call_history().size());
+    (void)mock.detach_interrupt();
+    expect(that % 1 == mock.spy_detach_interrupt.call_history().size());
+
+    // Exercise
+    mock.reset();
+
+    // Verify
+    expect(that % 0 == mock.spy_configure.call_history().size());
+    expect(that % 0 == mock.spy_attach_interrupt.call_history().size());
+    expect(that % 0 == mock.spy_detach_interrupt.call_history().size());
+  };
+};
+}  // namespace embed
