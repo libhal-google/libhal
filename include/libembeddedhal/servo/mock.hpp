b3f36ccf54616ad142b395a1ea5b010508db3ac9
MaliaLabor <malia.labor@hotmail.com>
2022-07-15 20:35:12 -0700

    Add embed::mock::servo
    Resolves #164

---
+++ include/libembeddedhal/servo/mock.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "../testing.hpp"
+#include "interface.hpp"
+
+namespace embed::mock {
+/**
+ * @brief mock servo implementation for use in unit tests and
+ * simulations.
+ *
+ */
+struct servo : public embed::servo
+{
+  /**
+   * @brief Reset spy information for position()
+   *
+   */
+  void reset()
+  {
+    spy_position.reset();
+  }
+
+  /// Spy handler for embed::servo::position()
+  spy_handler<embed::percent> spy_position;
+
+private:
+  boost::leaf::result<void> driver_position(
+    percent p_position) noexcept override
+  {
+    return spy_position.record(p_position);
+  }
+};
+}  // namespace embed::mock
