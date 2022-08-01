3023f670367239600df361bc8bbc2e5cd1da3e36
MaliaLabor <malia.labor@hotmail.com>
2022-08-01 14:55:54 -0700

    Replace percent with percentage in hal::motor
    Resolves #359

--- tests/motor/mock.test.cpp
+++ tests/motor/mock.test.cpp
@@ -7,18 +7,21 @@ boost::ut::suite motor_mock_test = []() {
 
   // Setup
   hal::mock::motor mock;
-  constexpr auto expected1 = percent::from_ratio(1, 2);
-  constexpr auto expected2 = percent::from_ratio(1, 4);
+  constexpr auto expected1 = percentage<config::float_type>(0.5);
+  constexpr auto expected2 = percentage<config::float_type>(0.25);
   mock.spy_power.trigger_error_on_call(3);
 
   // Exercise + Verify
   expect(bool{ mock.power(expected1) });
-  expect(expected1 == std::get<0>(mock.spy_power.call_history().at(0)));
+  expect(expected1.value() ==
+         std::get<0>(mock.spy_power.call_history().at(0)).value());
 
   expect(bool{ mock.power(expected2) });
-  expect(expected2 == std::get<0>(mock.spy_power.call_history().at(1)));
+  expect(expected2.value() ==
+         std::get<0>(mock.spy_power.call_history().at(1)).value());
 
   expect(!mock.power(expected2));
-  expect(expected2 == std::get<0>(mock.spy_power.call_history().at(2)));
+  expect(expected2.value() ==
+         std::get<0>(mock.spy_power.call_history().at(2)).value());
 };
 }  // namespace hal
