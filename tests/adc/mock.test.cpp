e5466d84965b442cf7b8b1577ae674d54ed4bf6e
MaliaLabor <malia.labor@hotmail.com>
2022-08-02 12:50:06 -0700

    Replace percent with percentage in hal::adc
    Resolves #364

--- tests/adc/mock.test.cpp
+++ tests/adc/mock.test.cpp
@@ -6,16 +6,16 @@ boost::ut::suite adc_mock_test = []() {
   using namespace boost::ut;
 
   // Setup
-  auto expected1 = hal::percent::from_ratio(1, 4);
-  auto expected2 = hal::percent::from_ratio(1, 2);
-  auto expected3 = hal::percent::from_ratio(1, 1);
+  auto expected1 = percentage<config::float_type>(0.25);
+  auto expected2 = percentage<config::float_type>(0.5);
+  auto expected3 = percentage<config::float_type>(1.0);
   hal::mock::adc mock(expected1);
 
   // Exercise + Verify
-  expect(that % expected1.raw_value() == mock.read().value().raw_value());
+  expect(that % expected1.value() == mock.read().value().value());
   mock.set(expected2);
-  expect(that % expected2.raw_value() == mock.read().value().raw_value());
+  expect(that % expected2.value() == mock.read().value().value());
   mock.set(expected3);
-  expect(that % expected3.raw_value() == mock.read().value().raw_value());
+  expect(that % expected3.value() == mock.read().value().value());
 };
 }  // namespace hal
