1a5c321c6783d86afea961d03867c42d71650d05
Khalil Estell <kammcecorp@gmail.com>
2022-08-01 19:29:15 -0700

    Add hal::map<float>()
    Resolves #338

--- tests/map.test.cpp
+++ tests/map.test.cpp
@@ -73,5 +73,50 @@ boost::ut::suite map_test = []() {
       }));
     };
   };
+
+  "hal::map<std::floating_point>()"_test = []() {
+    "Zero"_test = []() {
+      expect(that % 0.0_d ==
+             map(0.0, std::make_pair(0.0, 10.0), std::make_pair(0.0, 100.0)));
+    };
+    "Boundaries"_test = []() {
+      constexpr auto min = double{ std::numeric_limits<std::int32_t>::min() };
+      constexpr auto max = double{ std::numeric_limits<std::int32_t>::max() };
+
+      expect(that % 0.0_d ==
+             map(0.0, std::make_pair(min, max), std::make_pair(min, max)));
+
+      expect(that % 0.5_d ==
+             map(0.0, std::make_pair(min, max), std::make_pair(0.0, 1.0)));
+
+      expect(that % max == map(1338.0,
+                               std::make_pair(1337.0, 1338.0),
+                               std::make_pair(min, max)));
+
+      expect(that % min == map(1337.0,
+                               std::make_pair(1337.0, 1338.0),
+                               std::make_pair(min, max)));
+    };
+
+    "Standard usage"_test = []() {
+      expect(that % 50.0 ==
+             map(5.0, std::make_pair(0.0, 10.0), std::make_pair(0.0, 100.0)));
+      expect(that % 0.0 == map(5.0,
+                               std::make_pair(0.0, 10.0),
+                               std::make_pair(-100.0, 100.0)));
+      expect(that % 50.0 ==
+             map(-5.0, std::make_pair(-10.0, 0.0), std::make_pair(0.0, 100.0)));
+      expect(that % 25.0 == map(-75.0,
+                                std::make_pair(-100.0, 0.0),
+                                std::make_pair(0.0, 100.0)));
+      expect(that % -175.0 == map(-75.0,
+                                  std::make_pair(-100.0, 0.0),
+                                  std::make_pair(-200.0, -100.0)));
+      expect(that % 10.0 ==
+             map(0.0, std::make_pair(-10.0, 10.0), std::make_pair(10.0, 10.0)));
+      expect(that % 0.0 ==
+             map(0.0, std::make_pair(-1.0, 1.0), std::make_pair(-1.0, 1.0)));
+    };
+  };
 };
 }
