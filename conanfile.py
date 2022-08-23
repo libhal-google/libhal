742bf643e74f632bd89ef9a8fb592fd1f789246d
Khalil Estell <kammcecorp@gmail.com>
2022-08-23 08:09:55 -0700

    Fix conan files such that they conform to CCI
    CCI meaning conan center index requirements
    
    Issue #405

--- conanfile.py
+++ conanfile.py
@@ -3,18 +3,18 @@ from conans import ConanFile
 
 class libhal_conan(ConanFile):
     name = "libhal"
-    license = "Apache License Version 2.0"
-    url = "https://github.com/libhal/libhal"
+    license = "Apache-2.0"
+    url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://libhal.github.io/libhal"
     description = ("A collection of interfaces and abstractions for embedded "
                    "peripherals and devices using modern C++")
     topics = ("peripherals", "hardware", "abstraction", "devices")
     settings = "os", "compiler", "arch", "build_type"
-    exports_sources = "include/*", "LICENSE"
+    exports_sources = "include/*"
     no_copy_source = True
 
     def package(self):
-        self.copy("LICENSE", dst="licenses")
+        self.copy("*LICENSE*", dst="licenses", keep_path=False)
         self.copy("*.h")
         self.copy("*.hpp")
 
