b9b14603e1eeb66aca766b995e48ffcf43d763fc
Khalil Estell <kammcecorp@gmail.com>
2022-08-25 07:41:39 -0700

    Make conanfile.py similiar to libhal file on CCI
    - CCI = conan center index
    - Add C++20 & compiler version requirements
    - CCI and local conanfile.py difference is "export_sources" since CCI
      uses a conandata.yml file to download an archive file where as the
      repo version must pull the resources from our local directory.
    - Simplify test_package CMakeLists.txt, removing all GCC specific
      warning flags and the like.
    - Remove test_package tweaks file as the defaults will suffice.
    - Add conanfiles to gitignore
    
    Resolves #405

--- test_package/libhal.tweaks.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace hal::config {
-inline int callback_call_count = 0;
-constexpr bool on_error_callback_enabled = true;
-constexpr auto on_error_callback = []() { callback_call_count++; };
-// TODO(#404): Change this to "float" when the platform can support it
-using float_type = double;
-}  // namespace hal::config
