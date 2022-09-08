6e56f1f44af967b15877cc74992e5fa7f17ed722
MaliaLabor <malia.labor@hotmail.com>
2022-09-08 11:08:14 -0700

    Add mock::steady_clock
    Resolves #419

--- include/libhal/steady_clock/mock.hpp
+++ include/libhal/steady_clock/mock.hpp
@@ -9,7 +9,6 @@ namespace hal::mock {
  * @addtogroup steady_clock
  * @{
  */
-// TODO(#419): Fill this out
 /**
  * @brief mock steady_clock implementation for use in unit tests and
  * simulations.
@@ -17,7 +16,44 @@ namespace hal::mock {
  */
 struct steady_clock : public hal::steady_clock
 {
+  /**
+   * @brief Set the frequency to be returned from frequency()
+   *
+   * @param p_frequency - Frequency to return
+   */
+  void set_frequency(hertz p_frequency)
+  {
+    m_frequency = p_frequency;
+  }
+
+  /**
+   * @brief Queues the uptimes to be returned from uptimes()
+   *
+   * @param p_uptime_values - Queue of uptimes
+   */
+  void set_uptimes(std::queue<std::uint64_t>& p_uptime_values)
+  {
+    m_uptime_values = p_uptime_values;
+  }
+
 private:
+  hertz driver_frequency() noexcept
+  {
+    return m_frequency;
+  }
+
+  result<std::uint64_t> driver_uptime() noexcept
+  {
+    if (m_uptime_values.size() == 0) {
+      return hal::new_error(std::out_of_range("uptimes queue is empty!"));
+    }
+    std::uint64_t m_current_value = m_uptime_values.front();
+    m_uptime_values.pop();
+    return m_current_value;
+  }
+
+  hertz m_frequency = 1.0_Hz;
+  std::queue<std::uint64_t> m_uptime_values{};
 };
 /** @} */
 }  // namespace hal::mock
