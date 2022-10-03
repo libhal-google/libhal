fdb7e6b130ae7bf7176d9f03dce7e2f0751f7884
MaliaLabor <malia.labor@hotmail.com>
2022-10-03 08:58:00 -0700

    Add unit tests for interfaces
    Resolves #469

---
+++ tests/motor/interface.test.cpp
@@ -0,0 +1,52 @@
+#include <boost/ut.hpp>
+#include <libhal/motor/interface.hpp>
+
+namespace hal {
+namespace {
+constexpr auto expected_value = float(0.5);
+
+class test_motor : public hal::motor
+{
+public:
+  float m_power{};
+  bool m_return_error_status{ false };
+
+private:
+  status driver_power(float p_power) noexcept override
+  {
+    m_power = p_power;
+    if (m_return_error_status) {
+      return hal::new_error();
+    }
+    return success();
+  };
+};
+}  // namespace
+
+boost::ut::suite motor_test = []() {
+  using namespace boost::ut;
+  "motor interface test"_test = []() {
+    // Setup
+    test_motor test;
+
+    // Exercise
+    auto result = test.power(expected_value);
+
+    // Verify
+    expect(bool{ result });
+    expect(that % expected_value == test.m_power);
+  };
+
+  "motor errors test"_test = []() {
+    // Setup
+    test_motor test;
+    test.m_return_error_status = true;
+
+    // Exercise
+    auto result = test.power(expected_value);
+
+    // Verify
+    expect(!bool{ result });
+  };
+};
+}  // namespace hal
--- tests/servo/interface.test.cpp
+++ tests/servo/interface.test.cpp
@@ -11,26 +11,44 @@ class test_servo : public hal::servo
 {
 public:
   float m_passed_position{};
+  bool m_return_error_status{ false };
 
 private:
   status driver_position(float p_position) noexcept override
   {
     m_passed_position = p_position;
-    return {};
+    if (m_return_error_status) {
+      return hal::new_error();
+    }
+    return success();
   }
 };
 }  // namespace
 
 boost::ut::suite servo_test = []() {
   using namespace boost::ut;
-  // Setup
-  test_servo test;
+  "servo interface test"_test = []() {
+    // Setup
+    test_servo test;
 
-  // Exercise
-  auto result = test.position(expected_value);
+    // Exercise
+    auto result = test.position(expected_value);
 
-  // Verify
-  expect(bool{ result });
-  expect(that % expected_value == test.m_passed_position);
+    // Verify
+    expect(bool{ result });
+    expect(that % expected_value == test.m_passed_position);
+  };
+
+  "servo errors test"_test = []() {
+    // Setup
+    test_servo test;
+    test.m_return_error_status = true;
+
+    // Exercise
+    auto result = test.position(expected_value);
+
+    // Verify
+    expect(!bool{ result });
+  };
 };
 }  // namespace hal
