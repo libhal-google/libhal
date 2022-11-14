bf8f1d08d8d8ec651e69679bfbb4294758b3924b
MaliaLabor <malia.labor@hotmail.com>
2022-11-14 10:30:06 -0700

    Add hal::try_until
    Resolves #525

--- include/libhal/timeout.hpp
+++ include/libhal/timeout.hpp
@@ -9,6 +9,7 @@
 #include <functional>
 #include <system_error>
 
+#include "enum.hpp"
 #include "error.hpp"
 
 namespace hal {
@@ -31,6 +32,18 @@ template<class T>
 concept timeout = std::convertible_to<T, std::function<timeout_function>>;
 
 /**
+ * @brief Worker is a callable object or function that can be repeatedly tried
+ * with try_until
+ *
+ * @returns result<work_state> - sets error flag set when an error occurs,
+ * otherwise returns work_state enum.
+ */
+using work_function = result<work_state>();
+
+template<class T>
+concept worker = std::convertible_to<T, std::function<work_function>>;
+
+/**
  * @brief Delay the execution of the application or thread for a duration of
  * time.
  *
@@ -45,8 +58,7 @@ concept timeout = std::convertible_to<T, std::function<timeout_function>>;
   // This lambda catches a `std::errc::timed_out` handle them by changing
   // `waiting` from true to false in order to break the while loop below.
   auto timeout_catcher =
-    [&waiting](
-      boost::leaf::match<std::errc, std::errc::timed_out> p_errc) -> status {
+    [&waiting](hal::match<std::errc, std::errc::timed_out> p_errc) -> status {
     (void)p_errc;
     // Simply change the waiting bool
     waiting = false;
@@ -78,5 +90,25 @@ concept timeout = std::convertible_to<T, std::function<timeout_function>>;
 {
   return []() -> status { return {}; };
 }
+
+/**
+ * @brief Repeatedly call a worker function until a timeout is achieved
+ *
+ * @param p_worker - worker function to repeatedly call
+ * @param p_timeout - callable timeout object
+ * @return result<work_state> - state of the worker function
+ */
+inline result<work_state> try_until(worker auto p_worker,
+                                    timeout auto p_timeout) noexcept
+{
+  while (true) {
+    auto state = HAL_CHECK(p_worker());
+    if (state == work_state::failure || state == work_state::finished) {
+      return state;
+    }
+    HAL_CHECK(p_timeout());
+  }
+  return new_error(std::errc::state_not_recoverable);
+};
 /** @} */
 }  // namespace hal
