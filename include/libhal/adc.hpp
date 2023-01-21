e2d4a7d46c2967cfc5a4cfd2cbfba112c7d9b2be
Khalil Estell <kammcecorp@gmail.com>
2023-01-21 06:18:04 -0800

    :memo: Reword adc::read() comments

--- include/libhal/adc.hpp
+++ include/libhal/adc.hpp
@@ -9,17 +9,22 @@ namespace hal {
  *
  * Use this interface for devices and peripherals that can convert analog
  * voltage signals into a digital number.
+ *
+ * ADC peripheral only know the proportion of a voltage signal relative to a Vss
+ * (negative reference) and a Vcc (positive reference) and thus cannot describe
+ * the voltage directly.
  */
 class adc
 {
 public:
   /**
-   * @brief Read a sample from the analog to digital converter.
+   * @brief Sample the analog to digital converter and return the result
    *
-   * @return result<float> - float of the voltage from Vss to Vcc, where
-   * Vss is the negative or ground reference for the ADC and Vcc is the positive
-   * or supply voltage for the ADC. For example with a Vss of 0V and Vcc of 4V,
-   * a reading of 0.5 would represent 2V.
+   * @return result<float> - a value between 0.0f and +1.0f. The value
+   * representing the voltage measured by the ADC from Vss to Vcc, where Vss is
+   * the negative reference for the ADC and Vcc is the positive or supply
+   * voltage for the ADC. For example, if Vss is 0V and Vcc is 5V and this
+   * function returns 0.5f, then the voltage measured is 2.5V.
    */
   [[nodiscard]] result<float> read()
   {
