69743f25fe7582a05f53141a3a30728a022169b1
Khalil Estell <kammce@google.com>
2023-04-15 09:23:27 -0700

    :green_heart: Fix naming issues in tests

--- tests/helpers.hpp
+++ tests/helpers.hpp
@@ -1,13 +1,20 @@
 #pragma once
+
 #include <cmath>
 
-/** @brief Compares two single floating point values, with in a given error margin.
-* @param first_num first single  point value to compare
-* @param second_num second floating point value to compare
-* @param error_margin How precise the error of an error should be checked.
-* @return If the two floats are equal within a margin of error
-*/
-bool compare_floats(float first_num, float second_num, float error_margin = 0.0001f) {
-    float difference = abs(first_num - second_num);
-    return difference < error_margin;
+/**
+ * @brief Compares two single floating point values, with in a given error
+ * margin.
+ *
+ * @param p_first first single  point value to compare
+ * @param p_second second floating point value to compare
+ * @param p_error_margin How precise the error of an error should be checked.
+ * @return true if the two floats are equal within a margin of error
+ */
+bool compare_floats(float p_first,
+                    float p_second,
+                    float p_error_margin = 0.0001f)
+{
+  float difference = abs(p_first - p_second);
+  return difference < p_error_margin;
 }
