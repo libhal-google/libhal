8af061d4be76702ffe3ef8f4b7a9e5639bd5b1f8
Coreyboy1820 <92711317+Coreyboy1820@users.noreply.github.com>
2023-06-03 06:14:20 -0700

    :white_check_mark: Unit Testing Lengths UDL (#691)
    * :white_check_mark: Added Creation test, Equality, and calculation test for length UDL's
    
    * :white_check_mark: Added Creation test, Equality, and calculation test for length UDL's

---
+++ tests/helpers.cpp
@@ -0,0 +1,7 @@
+#include "helpers.hpp"
+
+bool compare_floats(float p_first, float p_second, float p_error_margin)
+{
+  float difference = abs(p_first - p_second);
+  return difference < p_error_margin;
+}
