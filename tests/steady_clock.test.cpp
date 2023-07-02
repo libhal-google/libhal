f2b633b34a4d20d1376333136ce978cb95ac5eea
Khalil Estell <kammce@google.com>
2023-07-02 07:00:33 -0700

    :bang: Remove result<T> from steady clock
    Version number will remain 2.0.0 as no users are currently using the
    software. The previous version will be purged from the server.
    
    This is to improve guarantees around steady clock, improve performance,
    and reduce code verbosity.

--- tests/steady_clock.test.cpp
+++ tests/steady_clock.test.cpp
@@ -23,23 +23,19 @@ class test_steady_clock : public hal::steady_clock
 public:
   constexpr static hertz m_frequency{ 1.0_Hz };
   constexpr static std::uint64_t m_uptime{ 100 };
-  bool m_return_error_status{ false };
 
   ~test_steady_clock()
   {
   }
 
 private:
-  result<frequency_t> driver_frequency() override
+  frequency_t driver_frequency() override
   {
     return frequency_t{ .operating_frequency = m_frequency };
   };
 
-  result<uptime_t> driver_uptime() override
+  uptime_t driver_uptime() override
   {
-    if (m_return_error_status) {
-      return hal::new_error();
-    }
     return uptime_t{ .ticks = m_uptime };
   };
 };
@@ -57,21 +53,8 @@ void steady_clock_test()
     auto result2 = test.uptime();
 
     // Verify
-    expect(that % test.m_frequency == result1.value().operating_frequency);
-    expect(bool{ result2 });
-    expect(that % test.m_uptime == result2.value().ticks);
-  };
-
-  "steady_clock errors test"_test = []() {
-    // Setup
-    test_steady_clock test;
-    test.m_return_error_status = true;
-
-    // Exercise
-    auto result = test.uptime();
-
-    // Verify
-    expect(!bool{ result });
+    expect(that % test.m_frequency == result1.operating_frequency);
+    expect(that % test.m_uptime == result2.ticks);
   };
 };
 }  // namespace hal
