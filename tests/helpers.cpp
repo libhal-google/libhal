9d3a84ff07dc46ddb2d7eddf836c38a9036dd2d2
Malia Labor <99438964+MaliaLabor@users.noreply.github.com>
2023-11-05 05:11:28 -0700

    :green_heart: Fix CI upgrade to libhal-cmake-util/3.0.0 (#708)

--- tests/helpers.cpp
+++ tests/helpers.cpp
@@ -1,6 +1,8 @@
 #include "helpers.hpp"
 
-bool compare_floats(float p_first, float p_second, float p_error_margin)
+bool compare_floats(float p_first,         // NOLINT
+                    float p_second,        // NOLINT
+                    float p_error_margin)  // NOLINT
 {
   float difference = std::abs(p_first - p_second);
   return difference < p_error_margin;
